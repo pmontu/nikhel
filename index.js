966447f2505bd368ae71a9dbfbce141534e3c632
pmontu <pmontu@gmail.com>
2015-12-15 20:10:04 +0530

    {message:"hello world"}

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db =  monk('localhost:27017/nikhel');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.json({message:"Hello World"})
 });
 
 app.listen(app.get('port'), function() {
