e26e3eebecf259e676093091be5ad4b52a2940ad
pmontu <pmontu@gmail.com>
2015-12-15 20:26:33 +0530

    users return []

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.json({message:"Hello World"})
+	res.json({message:"Hello World"})
+});
+
+app.get('/users/', function(req, res) {
+	var user = db.get("user")
+	user.find({},{},function(e, docs){
+		res.send(docs)
+	})
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+console.log('Node app is running on port', app.get('port'));
 });
 
 
